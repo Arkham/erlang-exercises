cbc117bc70eddce3a7809abc57a52ee9be73e9e6
Ju Liu <ju.liu@welaika.com>
2014-02-01 15:20:22 +0100

    made header functions tail recursive

--- rudy/http.erl
+++ rudy/http.erl
@@ -1,5 +1,5 @@
 -module(http).
--export([parse_request/1]).
+-export([parse_request/1, ok/1, get/1]).
 
 parse_request(R0) ->
   {Request, R1} = request_line(R0),
@@ -7,33 +7,45 @@ parse_request(R0) ->
   {Body, _} = message_body(R2),
   {Request, Headers, Body}.
 
+ok(Body) ->
+  "HTTP/1.1 200 OK\r\n" ++ "\r\n" ++ Body.
+
+get(URI) ->
+  "GET " ++ URI ++ " HTTP/1.1\r\n" ++ "\r\n".
+
 request_line([$G, $E, $T, 32 | R0]) ->
   {URI, R1} = request_uri(R0),
   {Ver, R2} = http_version(R1),
   [13,10|R3] = R2,
   {{get, URI, Ver}, R3}.
 
-request_uri([32|R0]) ->
-  {[], R0};
-request_uri([C|R0]) ->
-  {Rest, R1} = request_uri(R0),
-  {[C|Rest], R1}.
+request_uri(R0) ->
+  request_uri(R0, []).
+request_uri([32|R0], Acc) ->
+  {lists:reverse(Acc), R0};
+request_uri([C|R0], Acc) ->
+  request_uri(R0, [C|Acc]).
 
 http_version([$H, $T, $T, $P, $/, $1, $., $1 | R0]) ->
   {v11, R0};
 http_version([$H, $T, $T, $P, $/, $1, $., $0 | R0]) ->
   {v10, R0}.
 
-headers([13,10|R0]) ->
-  {[], R0};
 headers(R0) ->
+  headers(R0, []).
+headers([13,10|R0], Acc) ->
+  {lists:reverse(Acc), R0};
+headers(R0, Acc) ->
   {Header, R1} = header(R0),
-  {Rest, R2} = headers(R1),
-  {[Header|Rest], R2}.
+  headers(R1, [Header|Acc]).
 
-header([13,10|R0]) ->
-  {[], R0};
-header([C|R0]) ->
-  {Rest, R1} = header(R0),
-  {[C|Rest], R1}.
+header(R0) ->
+  header(R0, []).
+header([13,10|R0], Acc) ->
+  {lists:reverse(Acc), R0};
+header([C|R0], Acc) ->
+  header(R0, [C|Acc]).
+
+message_body(R) ->
+  {R, []}.
 
